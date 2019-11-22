33c6e7fde3a2cc2c4c35435ce569d82163bb9138
Jordan Brockopp <Jordan.Brockopp@target.com>
2019-11-22 08:49:37 -0600

    refactor(operator): cleanup logging implementation

--- cmd/server/operate.go
+++ cmd/server/operate.go
@@ -22,39 +22,52 @@ func operate(queue queue.Service, executors map[int]executor.Engine) (err error)
 		logrus.Infof("Thread ID %d listening to queue...", id)
 		threads.Go(func() error {
 			for {
+				// create the context for the executor
 				ctx := context.Background()
+
+				// pop an item from the queue
 				item, err := queue.Pop()
 				if err != nil {
 					return err
 				}
 
+				// create logger with extra metadata
+				logger := logrus.WithFields(logrus.Fields{
+					"build": item.Build.GetNumber(),
+					"repo":  item.Repo.GetFullName(),
+				})
+
+				// add build metadata to the executor
 				e.WithBuild(item.Build)
 				e.WithPipeline(item.Pipeline)
 				e.WithRepo(item.Repo)
 				e.WithUser(item.User)
 
-				logrus.Infof("creating %s build", item.Repo.GetFullName())
+				// create the build on the executor
+				logger.Infof("creating build")
 				err = e.CreateBuild(ctx)
 				if err != nil {
-					logrus.Errorf("unable to create build: %w", err)
+					logger.Errorf("unable to create build: %w", err)
 					return err
 				}
 
-				logrus.Infof("executing %s build", item.Repo.GetFullName())
+				// execute the build on the executor
+				logger.Infof("executing build")
 				err = e.ExecBuild(ctx)
 				if err != nil {
-					logrus.Errorf("unable to execute build: %w", err)
+					logger.Errorf("unable to execute build: %w", err)
 					return err
 				}
 
-				logrus.Infof("destroying %s build", item.Repo.GetFullName())
+				// destroy the build on the executor
+				logger.Info("destroying build")
 				err = e.DestroyBuild(ctx)
 				if err != nil {
-					logrus.Errorf("unable to destroy build: %w", err)
+					logger.Errorf("unable to destroy build: %w", err)
 					return err
 				}
 
-				logrus.Infof("completed %s build", item.Repo.GetFullName())
+				logger.Info("completed build")
 			}
 		})
 	}
