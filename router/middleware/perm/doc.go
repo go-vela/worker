2f2a5c95537b173a575f30c86430e9926fd644c9
Neal <neal.coleman@target.com>
2019-11-26 21:34:03 -0600

    feat(auth): Allow only server to make API calls with secret (#26)
    * feat(auth): Allow only server to make API calls with secret
    
    * Add godoc files
    
    * fix(err): catch error from gin
    
    * Fix(log): fix package name
    
    * fix(test): correct test data

---
+++ router/middleware/perm/doc.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2019 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+// Package perm provides the ability for inserting
+// Vela user permissions resources into or extracting Vela user permissions
+// resources from the middleware chain for the API.
+//
+// Usage:
+//
+// 	import "github.com/go-vela/worker/router/middleware/perm"
+
+package perm
---
+++ router/middleware/perm/perm.go
@@ -0,0 +1,35 @@
+// Copyright (c) 2019 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package perm
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/go-vela/types"
+	"github.com/go-vela/worker/router/middleware/user"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+// MustServer ensures the user is the vela server
+func MustServer() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		u := user.Retrieve(c)
+
+		if strings.EqualFold(u.GetName(), "vela-server") {
+			return
+		}
+
+		msg := fmt.Sprintf("User %s is not a platform admin", u.GetName())
+		err := c.Error(fmt.Errorf(msg))
+		if err != nil {
+			logrus.Error(err)
+		}
+		c.AbortWithStatusJSON(http.StatusUnauthorized, types.Error{Message: &msg})
+	}
+}
