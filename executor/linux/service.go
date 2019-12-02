8da6c82e67162f39d051cc640bfb0dbab97f85dc
Jordan Brockopp <jordan.brockopp@target.com>
2019-12-02 10:40:06 -0600

    fix(logs): upload detached container logs (#29)

--- executor/linux/service.go
+++ executor/linux/service.go
@@ -94,18 +94,6 @@ func (c *client) ExecService(ctx context.Context, ctn *pipeline.Container) error
 		"service": ctn.Name,
 	})
 
-	// run the container in a detached state
-	if ctn.Detach {
-		logger.Debug("running container in detach mode")
-		// run the runtime container
-		err := c.Runtime.RunContainer(ctx, c.pipeline, ctn)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	}
-
 	logger.Debug("running container")
 	// run the runtime container
 	err := c.Runtime.RunContainer(ctx, c.pipeline, ctn)
@@ -163,6 +151,11 @@ func (c *client) ExecService(ctx context.Context, ctn *pipeline.Container) error
 
 		return nil
 	}()
+
+	// do not wait for detached containers
+	if ctn.Detach {
+		return nil
+	}
 
 	logger.Debug("waiting for container")
 	// wait for the runtime container
