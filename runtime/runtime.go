9b001db3ead3bb8338ce52ef84fb0a7cfcdeab33
Jordan Brockopp <jordan.brockopp@target.com>
2019-12-03 09:51:53 -0600

    feat: add ability to inspect runtime resources (#31)
    * feat: enable inspecting runtime network
    
    * feat: enable inspecting runtime volume
    
    * improvement: update inspecting runtime container

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -17,9 +17,9 @@ type Engine interface {
 
 	// Container Engine interface functions
 
-	// InfoContainer defines a function that gets
-	// information on the pipeline container.
-	InfoContainer(context.Context, *pipeline.Container) error
+	// InspectContainer defines a function that inspects
+	// the pipeline container.
+	InspectContainer(context.Context, *pipeline.Container) ([]byte, error)
 	// RemoveContainer defines a function that deletes
 	// (kill, remove) the pipeline container.
 	RemoveContainer(context.Context, *pipeline.Container) error
@@ -41,6 +41,9 @@ type Engine interface {
 	// CreateNetwork defines a function that
 	// creates the pipeline network.
 	CreateNetwork(context.Context, *pipeline.Build) (string, error)
+	// InspectNetwork defines a function that
+	// inspects the pipeline network.
+	InspectNetwork(context.Context, *pipeline.Build) ([]byte, error)
 	// RemoveNetwork defines a function that
 	// deletes the pipeline network.
 	RemoveNetwork(context.Context, *pipeline.Build) error
@@ -50,6 +53,9 @@ type Engine interface {
 	// CreateVolume defines a function that
 	// creates the pipeline volume.
 	CreateVolume(context.Context, *pipeline.Build) (string, error)
+	// InspectVolume defines a function that
+	// inspects the pipeline volume.
+	InspectVolume(context.Context, *pipeline.Build) ([]byte, error)
 	// RemoveVolume defines a function that
 	// deletes the pipeline volume.
 	RemoveVolume(context.Context, *pipeline.Build) error
