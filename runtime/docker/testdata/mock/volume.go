f33fe906f8a8b9f15f17c8a6671423006b9738b5
Jordan Brockopp <jordan.brockopp@target.com>
2019-12-05 15:09:17 -0600

    test(docker): add tests for network/volume inspect (#37)

--- runtime/docker/testdata/mock/volume.go
+++ runtime/docker/testdata/mock/volume.go
@@ -12,14 +12,21 @@ import (
 )
 
 // helper function to specify the routes for the Docker Volume APIs
-func volumeRoutes(r *http.Request, p string) (*http.Response, error) {
+func volumeRoutes(r *http.Request, path string) (*http.Response, error) {
+
+	// get volume id
+	vol := ""
+	if strings.HasPrefix(path, "/volumes/") {
+		vol = strings.TrimPrefix(path, "/volumes/")
+	}
 
 	switch {
-	case strings.EqualFold(r.Method, http.MethodPost): // Path: /Volumes/create
+	case strings.EqualFold(r.Method, http.MethodPost): // Path: /volumes/create
 		return createVolume(r)
-	case strings.EqualFold(r.Method, http.MethodDelete): // Path: /Volumes/:Volume_id
-		id := strings.Split(p, "/")[1]
-		return removeVolume(r, id)
+	case strings.EqualFold(r.Method, http.MethodGet): // Path: /volumes/:volume_id
+		return inspectVolume(r, vol)
+	case strings.EqualFold(r.Method, http.MethodDelete): // Path: /volumes/:volume_id
+		return removeVolume(r, vol)
 	}
 
 	return nil, nil
@@ -41,10 +48,39 @@ func createVolume(r *http.Request) (*http.Response, error) {
 	}, nil
 }
 
-// helper function to return the mock results from an volume remove
-func removeVolume(r *http.Request, id string) (*http.Response, error) {
+// helper function to return the mock results from a network inspect
+func inspectVolume(r *http.Request, vol string) (*http.Response, error) {
+	logrus.Infof("Inspecting volume %s", vol)
 
-	logrus.Infof("Deleting a volume %s", id)
+	if strings.Contains(vol, "notfound") {
+		return &http.Response{
+			StatusCode: http.StatusNotFound,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte("volume not found"))),
+		}, nil
+	}
+
+	resp := types.Volume{
+		CreatedAt:  "2019-12-05T12:00:00Z",
+		Driver:     "local",
+		Labels:     map[string]string{},
+		Mountpoint: "/var/lib/docker/volumes/9c00b01ddf812433e804c72a139eadc69ae79396207e127737de5c917b9e89b2/_data",
+		Name:       "9c00b01ddf812433e804c72a139eadc69ae79396207e127737de5c917b9e89b2",
+		Options:    map[string]string{},
+		Scope:      "local",
+	}
+
+	b, _ := json.Marshal(resp)
+
+	return &http.Response{
+		StatusCode: http.StatusOK,
+		Body:       ioutil.NopCloser(bytes.NewReader(b)),
+	}, nil
+}
+
+// helper function to return the mock results from an volume remove
+func removeVolume(r *http.Request, vol string) (*http.Response, error) {
+
+	logrus.Infof("Deleting a volume %s", vol)
 	var b []byte
 
 	return &http.Response{
