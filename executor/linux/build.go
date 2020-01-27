1956f086cb6fbafcbe5089ea74bf547aa093c72a
Neal Coleman <Neal.Coleman@target.com>
2020-01-27 09:27:10 -0600

    fix: improper spelling in log output

--- executor/linux/build.go
+++ executor/linux/build.go
@@ -45,7 +45,7 @@ func (c *client) CreateBuild(ctx context.Context) error {
 		// update the build fields
 		b.SetFinished(time.Now().UTC().Unix())
 
-		c.logger.Info("uploading errorred stated")
+		c.logger.Info("uploading errorred state")
 		// send API call to update the build
 		_, _, err := c.Vela.Build.Update(r.GetOrg(), r.GetName(), b)
 		if err != nil {
@@ -313,7 +313,7 @@ func (c *client) ExecBuild(ctx context.Context) error {
 		// update the build fields
 		b.SetFinished(time.Now().UTC().Unix())
 
-		c.logger.Info("uploading errorred stated")
+		c.logger.Info("uploading errorred state")
 		// send API call to update the build
 		_, _, err := c.Vela.Build.Update(r.GetOrg(), r.GetName(), b)
 		if err != nil {
