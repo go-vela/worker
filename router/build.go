aca4c0284c5cdfeb1f80b07dd89a8b6848567b6d
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-03-26 14:28:40 -0500

    refactor(router): add comments and links

--- router/build.go
+++ router/build.go
@@ -9,16 +9,26 @@ import (
 	"github.com/go-vela/worker/api"
 )
 
-// buildHandlers is a function that extends the provided base router group
-// with the API handlers for build functionality.
+// BuildHandlers extends the provided base router group
+// by adding a collection of endpoints for handling
+// build related requests.
 //
-// GET    	/api/v1/executors/:executor/build
-// DELETE    /api/v1/executors/:executor/build/kill
-func buildHandlers(base *gin.RouterGroup) {
-	// builds endpoints
+// GET     /api/v1/executors/:executor/build
+// DELETE  /api/v1/executors/:executor/build/kill
+func BuildHandlers(base *gin.RouterGroup) {
+	// add a collection of endpoints for handling build related requests
+	//
+	// https://pkg.go.dev/github.com/gin-gonic/gin?tab=doc#RouterGroup.Group
 	build := base.Group("/build")
 	{
+		// add an endpoint for capturing the build
+		//
+		// https://pkg.go.dev/github.com/gin-gonic/gin?tab=doc#RouterGroup.GET
 		build.GET("", api.GetBuild)
+
+		// add an endpoint for killing the build
+		//
+		// https://pkg.go.dev/github.com/gin-gonic/gin?tab=doc#RouterGroup.DELETE
 		build.DELETE("/kill", api.KillBuild)
-	} // end of builds endpoints
+	}
 }
