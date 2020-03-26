d107946adc25ae5759880f04ec39575b4f43ecac
Jordan Brockopp <jordan.brockopp@target.com>
2020-03-26 14:52:09 -0500

    refactor(router): add comments and links (#72)

--- router/repo.go
+++ router/repo.go
@@ -9,14 +9,20 @@ import (
 	"github.com/go-vela/worker/api"
 )
 
-// repoHandlers is a function that extends the provided base router group
-// with the API handlers for repo functionality.
+// RepoHandlers extends the provided base router group
+// by adding a collection of endpoints for handling
+// repo related requests.
 //
-// GET    	/api/v1/executors/:executor/repo
-func repoHandlers(base *gin.RouterGroup) {
-	// repos endpoints
+// GET  /api/v1/executors/:executor/repo
+func RepoHandlers(base *gin.RouterGroup) {
+	// add a collection of endpoints for handling repo related requests
+	//
+	// https://pkg.go.dev/github.com/gin-gonic/gin?tab=doc#RouterGroup.Group
 	repo := base.Group("/repo")
 	{
+		// add an endpoint for capturing the repo
+		//
+		// https://pkg.go.dev/github.com/gin-gonic/gin?tab=doc#RouterGroup.GET
 		repo.GET("", api.GetRepo)
-	} // end of repos endpoints
+	}
 }
