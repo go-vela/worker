08a3fb6bb1dbf864c80cf7d41b2121f8f76a1812
Emmanuel Meinen <emmanuel.meinen@target.com>
2020-07-08 18:54:06 -0500

    feat(api): add open api (swagger) spec (#99)
    * swagger: add initial api spec
    
    * swagger: add api-spec
    
    * update api-spec, remove unneeded objects
    
    * name api-spec
    
    * go mod tidy
    
    * revert docker-compose change

--- api/executor.go
+++ api/executor.go
@@ -16,6 +16,35 @@ import (
 	exec "github.com/go-vela/worker/router/middleware/executor"
 )
 
+// swagger:operation GET /api/v1/executors/{executor} executor GetExecutor
+//
+// Get a currently running executor
+//
+// ---
+// x-success_http_code: '200'
+// produces:
+// - application/json
+// parameters:
+// - in: header
+//   name: Authorization
+//   description: Vela server token
+//   required: true
+//   type: string
+// - in: path
+//   name: executor
+//   description: The executor to retrieve
+//   required: true
+//   type: string
+// responses:
+//   '200':
+//     description: Successfully retrieved the executor
+//     type: json
+//     schema:
+//       "$ref": "#/definitions/Executor"
+//   '500':
+//     description: Unable to retrieve the executor
+//     type: json
+
 // GetExecutor represents the API handler to capture the
 // executor currently running on a worker.
 func GetExecutor(c *gin.Context) {
@@ -61,6 +90,30 @@ func GetExecutor(c *gin.Context) {
 
 	c.JSON(http.StatusOK, executor)
 }
+
+// swagger:operation GET /api/v1/executors executor GetExecutors
+//
+// Get all currently running executors
+//
+// ---
+// x-success_http_code: '200'
+// produces:
+// - application/json
+// parameters:
+// - in: header
+//   name: Authorization
+//   description: Vela server token
+//   required: true
+//   type: string
+// responses:
+//   '200':
+//     description: Successfully retrieved all running executors
+//     type: json
+//     schema:
+//       "$ref": "#/definitions/Executor"
+//   '500':
+//     description: Unable to retrieve all running executors
+//     type: json
 
 // GetExecutors represents the API handler to capture the
 // executors currently running on a worker.
--- api/health.go
+++ api/health.go
@@ -10,6 +10,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// swagger:operation GET /health system Health
+//
+// Check if the worker API is available
+//
+// ---
+// x-success_http_code: '200'
+// produces:
+// - application/json
+// parameters:
+// responses:
+//   '200':
+//     description: Successful 'ping' of Vela worker API
+//     schema:
+//       type: string
+
 // Health check the status of the application
 func Health(c *gin.Context) {
 	c.JSON(http.StatusOK, "ok")
