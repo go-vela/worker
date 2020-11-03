74365ad6ce864c18b0b6b3505c0d229eb14f1f02
EmmanuelMeinen <Emmanuel.Meinen@target.com>
2020-11-03 15:31:16 -0600

    fix: correct and improve register error handling

--- cmd/vela-worker/register.go
+++ cmd/vela-worker/register.go
@@ -5,6 +5,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/go-vela/types/library"
 	"github.com/sirupsen/logrus"
 )
@@ -12,25 +14,33 @@ import (
 // register is a helper function to register
 // the worker in the database, updating the item
 // if the worker already exists
-func (w *Worker) register(config *library.Worker) error {
+func (w *Worker) register(config *library.Worker) {
 	// check to see if the worker already exists in the database
-	_, _, err := w.VelaClient.Worker.Get(config.GetHostname())
-	if err != nil {
+	_, resp, err := w.VelaClient.Worker.Get(config.GetHostname())
+	if resp.StatusCode == http.StatusNotFound {
 		// worker does not exist, create it
 		logrus.Info("registering worker with the server")
 		_, _, err := w.VelaClient.Worker.Add(config)
 		if err != nil {
-			return err
+			// log the error instead of returning so the operation doesn't block worker deployment
+			logrus.Error("unable to register worker with the server: %w", err)
 		}
-		return nil
+		return
+	}
+
+	// if there was an error other than a 404, log the error.
+	if err != nil {
+		// log the error instead of returning so the operation doesn't block worker deployment
+		logrus.Error("unable to get worker from server: %w", err)
 	}
 
 	// the worker exists in the db, update it with the new config
 	logrus.Info("worker previously registered with server, updating information")
 	_, _, err = w.VelaClient.Worker.Update(config.GetHostname(), config)
 	if err != nil {
-		return err
+		// log the error instead of returning so the operation doesn't block worker deployment
+		logrus.Error("unable to update worker on the server: %w", err)
 	}
 
-	return nil
+	return
 }
