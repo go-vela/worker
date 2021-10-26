fceece16a9dd0de490a24b14012644323c98b175
Jordan Brockopp <jordan.brockopp@target.com>
2021-10-26 11:56:16 -0500

    feat(internal): add internal logic from pkg-executor (#219)
    * feat(internal): add logic from pkg-executor
    
    * chore: clean go dependencies
    
    * chore: address linter feedback

---
+++ internal/internal.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package internal
+
+// For more information:
+//
+//   * https://golang.org/doc/go1.4#internalpackages
+//   * https://docs.google.com/document/d/1e8kOo3r51b2BWtTs_1uADIA5djfXhPT36s6eHVRIvaU/edit
