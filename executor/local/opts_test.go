ded6d3d2ac2be41884d14fb3919873f240422762
Jordan Brockopp <jordan.brockopp@target.com>
2021-11-01 10:11:10 -0500

    refactor: pull server mock package from server (#230)

--- executor/local/opts_test.go
+++ executor/local/opts_test.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	"github.com/go-vela/mock/server"
+	"github.com/go-vela/server/mock/server"
 
 	"github.com/go-vela/worker/runtime"
 	"github.com/go-vela/worker/runtime/docker"
