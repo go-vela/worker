0e47493ee7ffc9e8b16f5f6dc925c1e658ad026f
JordanSussman <jordansail22@gmail.com>
2022-02-28 23:04:10 -0600

    enhance(http): add retry client

--- cmd/vela-worker/client.go
+++ cmd/vela-worker/client.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"github.com/go-vela/sdk-go/vela"
+	"github.com/hashicorp/go-retryablehttp"
 
 	"github.com/sirupsen/logrus"
 )
@@ -13,11 +14,15 @@ import (
 // helper function to setup the queue from the CLI arguments.
 func setupClient(s *Server) (*vela.Client, error) {
 	logrus.Debug("creating vela client from worker configuration")
+	// create client to retryable http calls
+	retryClient := retryablehttp.NewClient()
+	// set logger to nil to avoid spam
+	retryClient.Logger = nil
 
 	// create a new Vela client from the server configuration
 	//
 	// https://pkg.go.dev/github.com/go-vela/sdk-go/vela?tab=doc#NewClient
-	vela, err := vela.NewClient(s.Address, "", nil)
+	vela, err := vela.NewClient(s.Address, "", retryClient.StandardClient())
 	if err != nil {
 		return nil, err
 	}
