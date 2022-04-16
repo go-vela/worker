2b1c39ec37d57336349b644d29c65892e7422065
Jacob Floyd <cognifloyd@gmail.com>
2022-04-15 22:12:46 -0500

    chore: lint fixes

--- runtime/kubernetes/build_test.go
+++ runtime/kubernetes/build_test.go
@@ -392,6 +392,7 @@ func TestKubernetes_AssembleBuild(t *testing.T) {
 	for _, test := range tests {
 		_engine, err := NewMock(test.k8sPod)
 		_engine.Pod = test.enginePod
+
 		_engine.containersLookup = map[string]int{}
 		for i, ctn := range test.enginePod.Spec.Containers {
 			_engine.containersLookup[ctn.Name] = i
