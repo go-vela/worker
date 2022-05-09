01d6942ef7eb3330ed0144bbee899dd21b016e8a
Jacob Floyd <cognifloyd@gmail.com>
2022-05-09 17:05:03 -0500

    tests: add integration test for exec()

---
+++ mock/worker/repo.go
@@ -0,0 +1,62 @@
+// Copyright (c) 2022 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package worker
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-vela/types"
+	"github.com/go-vela/types/library"
+)
+
+const (
+	// RepoResp represents a JSON return for a single repo.
+	RepoResp = `{
+  "id": 1,
+  "user_id": 1,
+  "org": "github",
+  "name": "octocat",
+  "full_name": "github/octocat",
+  "link": "https://github.com/github/octocat",
+  "clone": "https://github.com/github/octocat",
+  "branch": "master",
+  "build_limit": 10,
+  "timeout": 60,
+  "visibility": "public",
+  "private": false,
+  "trusted": true,
+  "active": true,
+  "allow_pr": false,
+  "allow_push": true,
+  "allow_deploy": false,
+  "allow_tag": false
+}`
+)
+
+// getRepo has a param :repo returns mock JSON for a http GET.
+//
+// Pass "not-found" to :repo to test receiving a http 404 response.
+func getRepo(c *gin.Context) {
+	r := c.Param("repo")
+
+	if strings.Contains(r, "not-found") {
+		msg := fmt.Sprintf("Repo %s does not exist", r)
+
+		c.AbortWithStatusJSON(http.StatusNotFound, types.Error{Message: &msg})
+
+		return
+	}
+
+	data := []byte(RepoResp)
+
+	var body library.Repo
+	_ = json.Unmarshal(data, &body)
+
+	c.JSON(http.StatusOK, body)
+}
