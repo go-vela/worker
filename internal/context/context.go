b27f24a65e5245a8fd6bbbddcd9b2a24b99a391e
Jacob Floyd <cognifloyd@gmail.com>
2022-10-10 18:36:30 -0500

    enhance: add log messages to WithDelayedCancelPropagation

--- internal/context/context.go
+++ internal/context/context.go
@@ -7,9 +7,11 @@ package context
 import (
 	"context"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
-func WithDelayedCancelPropagation(parent context.Context, timeout time.Duration) (context.Context, context.CancelFunc) {
+func WithDelayedCancelPropagation(parent context.Context, timeout time.Duration, name string, logger *logrus.Entry) (context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	go func() {
@@ -18,18 +20,27 @@ func WithDelayedCancelPropagation(parent context.Context, timeout time.Duration)
 		// start the timer once the parent context is canceled
 		select {
 		case <-parent.Done():
+			logger.Tracef("parent context is done, starting %s timer for %s", name, timeout)
 			timer = time.NewTimer(timeout)
+
+			break
 		case <-ctx.Done():
+			logger.Tracef("%s finished before the parent context", name)
+
 			return
 		}
 
 		// wait for the timer to elapse or the context to naturally finish.
 		select {
 		case <-timer.C:
+			logger.Tracef("%s timed out, propagating cancel to %s context", name, name)
 			cancel()
+
 			return
 		case <-ctx.Done():
+			logger.Tracef("%s finished, stopping timeout timer", name)
 			timer.Stop()
+
 			return
 		}
 	}()
