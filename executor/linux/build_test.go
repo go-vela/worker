af8760fc3c4a8184c4cc7f700e620d79d40ea37d
davidvader <davelvader@gmail.com>
2022-11-30 08:55:10 -0600

    fix: move test

--- executor/linux/build_test.go
+++ executor/linux/build_test.go
@@ -130,248 +130,6 @@ func TestLinux_CreateBuild(t *testing.T) {
 	}
 }
 
-func TestLinux_PlanBuild(t *testing.T) {
-	// setup types
-	compiler, _ := native.New(cli.NewContext(nil, flag.NewFlagSet("test", 0), nil))
-
-	_build := testBuild()
-	_repo := testRepo()
-	_user := testUser()
-	_metadata := testMetadata()
-
-	gin.SetMode(gin.TestMode)
-
-	s := httptest.NewServer(server.FakeHandler())
-
-	_client, err := vela.NewClient(s.URL, "", nil)
-	if err != nil {
-		t.Errorf("unable to create Vela API client: %v", err)
-	}
-
-	_runtime, err := docker.NewMock()
-	if err != nil {
-		t.Errorf("unable to create runtime engine: %v", err)
-	}
-
-	tests := []struct {
-		name     string
-		failure  bool
-		pipeline string
-	}{
-		{
-			name:     "basic secrets pipeline",
-			failure:  false,
-			pipeline: "testdata/build/secrets/basic.yml",
-		},
-		{
-			name:     "basic services pipeline",
-			failure:  false,
-			pipeline: "testdata/build/services/basic.yml",
-		},
-		{
-			name:     "basic steps pipeline",
-			failure:  false,
-			pipeline: "testdata/build/steps/basic.yml",
-		},
-		{
-			name:     "basic stages pipeline",
-			failure:  false,
-			pipeline: "testdata/build/stages/basic.yml",
-		},
-	}
-
-	// run test
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			_pipeline, _, err := compiler.
-				Duplicate().
-				WithBuild(_build).
-				WithRepo(_repo).
-				WithMetadata(_metadata).
-				WithUser(_user).
-				Compile(test.pipeline)
-			if err != nil {
-				t.Errorf("unable to compile pipeline %s: %v", test.pipeline, err)
-			}
-
-			_engine, err := New(
-				WithBuild(_build),
-				WithPipeline(_pipeline),
-				WithRepo(_repo),
-				WithRuntime(_runtime),
-				WithUser(_user),
-				WithVelaClient(_client),
-			)
-			if err != nil {
-				t.Errorf("unable to create executor engine: %v", err)
-			}
-
-			// run create to init steps to be created properly
-			err = _engine.CreateBuild(context.Background())
-			if err != nil {
-				t.Errorf("unable to create build: %v", err)
-			}
-
-			err = _engine.PlanBuild(context.Background())
-
-			if test.failure {
-				if err == nil {
-					t.Errorf("PlanBuild should have returned err")
-				}
-
-				return // continue to next test
-			}
-
-			if err != nil {
-				t.Errorf("PlanBuild returned err: %v", err)
-			}
-		})
-	}
-}
-
-func TestLinux_AssembleBuild(t *testing.T) {
-	// setup types
-	compiler, _ := native.New(cli.NewContext(nil, flag.NewFlagSet("test", 0), nil))
-
-	_build := testBuild()
-	_repo := testRepo()
-	_user := testUser()
-	_metadata := testMetadata()
-
-	gin.SetMode(gin.TestMode)
-
-	s := httptest.NewServer(server.FakeHandler())
-
-	_client, err := vela.NewClient(s.URL, "", nil)
-	if err != nil {
-		t.Errorf("unable to create Vela API client: %v", err)
-	}
-
-	_runtime, err := docker.NewMock()
-	if err != nil {
-		t.Errorf("unable to create runtime engine: %v", err)
-	}
-
-	streamRequests, done := message.MockStreamRequestsWithCancel(context.Background())
-	defer done()
-
-	tests := []struct {
-		name     string
-		failure  bool
-		pipeline string
-	}{
-		{
-			name:     "basic secrets pipeline",
-			failure:  false,
-			pipeline: "testdata/build/secrets/basic.yml",
-		},
-		{
-			name:     "secrets pipeline with image not found",
-			failure:  true,
-			pipeline: "testdata/build/secrets/img_notfound.yml",
-		},
-		{
-			name:     "secrets pipeline with ignoring image not found",
-			failure:  true,
-			pipeline: "testdata/build/secrets/img_ignorenotfound.yml",
-		},
-		{
-			name:     "basic services pipeline",
-			failure:  false,
-			pipeline: "testdata/build/services/basic.yml",
-		},
-		{
-			name:     "services pipeline with image not found",
-			failure:  true,
-			pipeline: "testdata/build/services/img_notfound.yml",
-		},
-		{
-			name:     "services pipeline with ignoring image not found",
-			failure:  true,
-			pipeline: "testdata/build/services/img_ignorenotfound.yml",
-		},
-		{
-			name:     "basic steps pipeline",
-			failure:  false,
-			pipeline: "testdata/build/steps/basic.yml",
-		},
-		{
-			name:     "steps pipeline with image not found",
-			failure:  true,
-			pipeline: "testdata/build/steps/img_notfound.yml",
-		},
-		{
-			name:     "steps pipeline with ignoring image not found",
-			failure:  true,
-			pipeline: "testdata/build/steps/img_ignorenotfound.yml",
-		},
-		{
-			name:     "basic stages pipeline",
-			failure:  false,
-			pipeline: "testdata/build/stages/basic.yml",
-		},
-		{
-			name:     "stages pipeline with image not found",
-			failure:  true,
-			pipeline: "testdata/build/stages/img_notfound.yml",
-		},
-		{
-			name:     "stages pipeline with ignoring image not found",
-			failure:  true,
-			pipeline: "testdata/build/stages/img_ignorenotfound.yml",
-		},
-	}
-
-	// run test
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			_pipeline, _, err := compiler.
-				Duplicate().
-				WithBuild(_build).
-				WithRepo(_repo).
-				WithMetadata(_metadata).
-				WithUser(_user).
-				Compile(test.pipeline)
-			if err != nil {
-				t.Errorf("unable to compile pipeline %s: %v", test.pipeline, err)
-			}
-
-			_engine, err := New(
-				WithBuild(_build),
-				WithPipeline(_pipeline),
-				WithRepo(_repo),
-				WithRuntime(_runtime),
-				WithUser(_user),
-				WithVelaClient(_client),
-				withStreamRequests(streamRequests),
-			)
-			if err != nil {
-				t.Errorf("unable to create executor engine: %v", err)
-			}
-
-			// run create to init steps to be created properly
-			err = _engine.CreateBuild(context.Background())
-			if err != nil {
-				t.Errorf("unable to create build: %v", err)
-			}
-
-			err = _engine.AssembleBuild(context.Background())
-
-			if test.failure {
-				if err == nil {
-					t.Errorf("AssembleBuild should have returned err")
-				}
-
-				return // continue to next test
-			}
-
-			if err != nil {
-				t.Errorf("AssembleBuild returned err: %v", err)
-			}
-		})
-	}
-}
-
 func TestLinux_AssembleBuild_EnforceTrustedRepos(t *testing.T) {
 	// setup types
 	compiler, _ := native.New(cli.NewContext(nil, flag.NewFlagSet("test", 0), nil))
@@ -863,6 +621,248 @@ func TestLinux_AssembleBuild_EnforceTrustedRepos(t *testing.T) {
 			err = _engine.CreateBuild(context.Background())
 			if err != nil {
 				t.Errorf("CreateBuild returned err: %v", err)
+			}
+
+			err = _engine.AssembleBuild(context.Background())
+
+			if test.failure {
+				if err == nil {
+					t.Errorf("AssembleBuild should have returned err")
+				}
+
+				return // continue to next test
+			}
+
+			if err != nil {
+				t.Errorf("AssembleBuild returned err: %v", err)
+			}
+		})
+	}
+}
+
+func TestLinux_PlanBuild(t *testing.T) {
+	// setup types
+	compiler, _ := native.New(cli.NewContext(nil, flag.NewFlagSet("test", 0), nil))
+
+	_build := testBuild()
+	_repo := testRepo()
+	_user := testUser()
+	_metadata := testMetadata()
+
+	gin.SetMode(gin.TestMode)
+
+	s := httptest.NewServer(server.FakeHandler())
+
+	_client, err := vela.NewClient(s.URL, "", nil)
+	if err != nil {
+		t.Errorf("unable to create Vela API client: %v", err)
+	}
+
+	_runtime, err := docker.NewMock()
+	if err != nil {
+		t.Errorf("unable to create runtime engine: %v", err)
+	}
+
+	tests := []struct {
+		name     string
+		failure  bool
+		pipeline string
+	}{
+		{
+			name:     "basic secrets pipeline",
+			failure:  false,
+			pipeline: "testdata/build/secrets/basic.yml",
+		},
+		{
+			name:     "basic services pipeline",
+			failure:  false,
+			pipeline: "testdata/build/services/basic.yml",
+		},
+		{
+			name:     "basic steps pipeline",
+			failure:  false,
+			pipeline: "testdata/build/steps/basic.yml",
+		},
+		{
+			name:     "basic stages pipeline",
+			failure:  false,
+			pipeline: "testdata/build/stages/basic.yml",
+		},
+	}
+
+	// run test
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_pipeline, _, err := compiler.
+				Duplicate().
+				WithBuild(_build).
+				WithRepo(_repo).
+				WithMetadata(_metadata).
+				WithUser(_user).
+				Compile(test.pipeline)
+			if err != nil {
+				t.Errorf("unable to compile pipeline %s: %v", test.pipeline, err)
+			}
+
+			_engine, err := New(
+				WithBuild(_build),
+				WithPipeline(_pipeline),
+				WithRepo(_repo),
+				WithRuntime(_runtime),
+				WithUser(_user),
+				WithVelaClient(_client),
+			)
+			if err != nil {
+				t.Errorf("unable to create executor engine: %v", err)
+			}
+
+			// run create to init steps to be created properly
+			err = _engine.CreateBuild(context.Background())
+			if err != nil {
+				t.Errorf("unable to create build: %v", err)
+			}
+
+			err = _engine.PlanBuild(context.Background())
+
+			if test.failure {
+				if err == nil {
+					t.Errorf("PlanBuild should have returned err")
+				}
+
+				return // continue to next test
+			}
+
+			if err != nil {
+				t.Errorf("PlanBuild returned err: %v", err)
+			}
+		})
+	}
+}
+
+func TestLinux_AssembleBuild(t *testing.T) {
+	// setup types
+	compiler, _ := native.New(cli.NewContext(nil, flag.NewFlagSet("test", 0), nil))
+
+	_build := testBuild()
+	_repo := testRepo()
+	_user := testUser()
+	_metadata := testMetadata()
+
+	gin.SetMode(gin.TestMode)
+
+	s := httptest.NewServer(server.FakeHandler())
+
+	_client, err := vela.NewClient(s.URL, "", nil)
+	if err != nil {
+		t.Errorf("unable to create Vela API client: %v", err)
+	}
+
+	_runtime, err := docker.NewMock()
+	if err != nil {
+		t.Errorf("unable to create runtime engine: %v", err)
+	}
+
+	streamRequests, done := message.MockStreamRequestsWithCancel(context.Background())
+	defer done()
+
+	tests := []struct {
+		name     string
+		failure  bool
+		pipeline string
+	}{
+		{
+			name:     "basic secrets pipeline",
+			failure:  false,
+			pipeline: "testdata/build/secrets/basic.yml",
+		},
+		{
+			name:     "secrets pipeline with image not found",
+			failure:  true,
+			pipeline: "testdata/build/secrets/img_notfound.yml",
+		},
+		{
+			name:     "secrets pipeline with ignoring image not found",
+			failure:  true,
+			pipeline: "testdata/build/secrets/img_ignorenotfound.yml",
+		},
+		{
+			name:     "basic services pipeline",
+			failure:  false,
+			pipeline: "testdata/build/services/basic.yml",
+		},
+		{
+			name:     "services pipeline with image not found",
+			failure:  true,
+			pipeline: "testdata/build/services/img_notfound.yml",
+		},
+		{
+			name:     "services pipeline with ignoring image not found",
+			failure:  true,
+			pipeline: "testdata/build/services/img_ignorenotfound.yml",
+		},
+		{
+			name:     "basic steps pipeline",
+			failure:  false,
+			pipeline: "testdata/build/steps/basic.yml",
+		},
+		{
+			name:     "steps pipeline with image not found",
+			failure:  true,
+			pipeline: "testdata/build/steps/img_notfound.yml",
+		},
+		{
+			name:     "steps pipeline with ignoring image not found",
+			failure:  true,
+			pipeline: "testdata/build/steps/img_ignorenotfound.yml",
+		},
+		{
+			name:     "basic stages pipeline",
+			failure:  false,
+			pipeline: "testdata/build/stages/basic.yml",
+		},
+		{
+			name:     "stages pipeline with image not found",
+			failure:  true,
+			pipeline: "testdata/build/stages/img_notfound.yml",
+		},
+		{
+			name:     "stages pipeline with ignoring image not found",
+			failure:  true,
+			pipeline: "testdata/build/stages/img_ignorenotfound.yml",
+		},
+	}
+
+	// run test
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_pipeline, _, err := compiler.
+				Duplicate().
+				WithBuild(_build).
+				WithRepo(_repo).
+				WithMetadata(_metadata).
+				WithUser(_user).
+				Compile(test.pipeline)
+			if err != nil {
+				t.Errorf("unable to compile pipeline %s: %v", test.pipeline, err)
+			}
+
+			_engine, err := New(
+				WithBuild(_build),
+				WithPipeline(_pipeline),
+				WithRepo(_repo),
+				WithRuntime(_runtime),
+				WithUser(_user),
+				WithVelaClient(_client),
+				withStreamRequests(streamRequests),
+			)
+			if err != nil {
+				t.Errorf("unable to create executor engine: %v", err)
+			}
+
+			// run create to init steps to be created properly
+			err = _engine.CreateBuild(context.Background())
+			if err != nil {
+				t.Errorf("unable to create build: %v", err)
 			}
 
 			err = _engine.AssembleBuild(context.Background())
