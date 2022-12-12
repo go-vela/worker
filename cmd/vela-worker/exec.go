cfb2598228ad2db2fab166b18c3ef2ab082d8951
wass3r <wasser@gmail.com>
2022-12-12 14:21:44 -0600

    fix header

--- cmd/vela-worker/exec.go
+++ cmd/vela-worker/exec.go
@@ -1,4 +1,6 @@
-// Copyright (c) 2022 Target Brands, Inc. All rights reserved. Use of this source code is governed by the LICENSE file in this repository.
+// Copyright (c) 2022 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
 
 package main
 
