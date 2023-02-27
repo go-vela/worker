95f4223a1b23ee45c1c5f6acd0f0a78f50d14c62
Jacob Floyd <cognifloyd@gmail.com>
2023-02-27 12:07:53 -0600

    enhance(executor tests):  Manage k8s mocks for Executor AssembleBuild test (#432)
    * test: Fix kubernetes tests for executor AssembleBuild
    
    Adds some new WaitForPod* test helpers.
    
    * sort imports in runtime/kubernetes/kubernetes.go
    
    * test: add pause to simulate step execution
    
    * AssembleBuild test does not need to mark steps running

--- executor/linux/build_test.go
+++ executor/linux/build_test.go
@@ -1305,8 +1305,7 @@ func TestLinux_AssembleBuild(t *testing.T) {
 
 			switch test.runtime {
 			case constants.DriverKubernetes:
-				_pod := testPodFor(_pipeline)
-				_runtime, err = kubernetes.NewMock(_pod)
+				_runtime, err = kubernetes.NewMock(&v1.Pod{}) // do not use _pod here! AssembleBuild will load it.
 				if err != nil {
 					t.Errorf("unable to create kubernetes runtime engine: %v", err)
 				}
@@ -1335,6 +1334,30 @@ func TestLinux_AssembleBuild(t *testing.T) {
 			err = _engine.CreateBuild(context.Background())
 			if err != nil {
 				t.Errorf("unable to create build: %v", err)
+			}
+
+			// Kubernetes runtime needs to set up the Mock after CreateBuild is called
+			if test.runtime == constants.DriverKubernetes {
+				go func() {
+					_mockRuntime := _runtime.(kubernetes.MockKubernetesRuntime)
+					// This handles waiting until runtime.AssembleBuild has prepared the PodTracker.
+					_mockRuntime.WaitForPodTrackerReady()
+					// Normally, runtime.StreamBuild (which runs in a goroutine) calls PodTracker.Start.
+					_mockRuntime.StartPodTracker(context.Background())
+
+					_pod := testPodFor(_pipeline)
+
+					// Now wait until the pod is created at the end of runtime.AssembleBuild.
+					_mockRuntime.WaitForPodCreate(_pod.GetNamespace(), _pod.GetName())
+
+					// Mark services running and secrets as completed, but no steps have started.
+					err := _mockRuntime.SimulateStatusUpdate(_pod,
+						testContainerStatuses(_pipeline, true, 0, 0),
+					)
+					if err != nil {
+						t.Errorf("%s - failed to simulate pod update: %s", test.name, err)
+					}
+				}()
 			}
 
 			err = _engine.AssembleBuild(context.Background())
