21eece0d21fb3a1c272d6221683ae1558f638023
David May <49894298+wass3rw3rk@users.noreply.github.com>
2023-04-19 17:09:36 -0500

    fix(register): verify hostname (#465)

--- api/register.go
+++ api/register.go
@@ -5,10 +5,12 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-vela/worker/router/middleware/token"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 // swagger:operation POST /register system Register
@@ -39,6 +41,13 @@ import (
 // channel of the worker. This will unblock operation if the worker has not been
 // registered and the provided registration token is valid.
 func Register(c *gin.Context) {
+	// extract the worker hostname that was packed into gin context
+	w, ok := c.Get("worker-hostname")
+	if !ok {
+		c.JSON(http.StatusInternalServerError, "no worker hostname in the context")
+		return
+	}
+
 	// extract the register token channel that was packed into gin context
 	v, ok := c.Get("register-token")
 	if !ok {
@@ -68,8 +77,53 @@ func Register(c *gin.Context) {
 		return
 	}
 
+	// extract the subject from the token
+	sub, err := getSubjectFromToken(token)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, err)
+		return
+	}
+
+	// make sure we configured the hostname properly
+	hostname, ok := w.(string)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, "worker hostname in the context is the wrong type")
+		return
+	}
+
+	// if the subject doesn't match the worker hostname return an error
+	if sub != hostname {
+		c.JSON(http.StatusUnauthorized, "worker hostname is invalid")
+		return
+	}
+
 	// write registration token to auth token channel
 	rChan <- token
 
 	c.JSON(http.StatusOK, "successfully passed token to worker")
+}
+
+// getSubjectFromToken is a helper function to extract
+// the subject from the token claims.
+func getSubjectFromToken(token string) (string, error) {
+	// create a new JWT parser
+	j := jwt.NewParser()
+
+	// parse the payload
+	t, _, err := j.ParseUnverified(token, jwt.MapClaims{})
+	if err != nil {
+		return "", fmt.Errorf("unable to parse token")
+	}
+
+	sub, err := t.Claims.GetSubject()
+	if err != nil {
+		return "", fmt.Errorf("unable to get subject from token")
+	}
+
+	// make sure there was a subject defined
+	if len(sub) == 0 {
+		return "", fmt.Errorf("no subject defined in token")
+	}
+
+	return sub, nil
 }
