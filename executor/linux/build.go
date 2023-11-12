f97d3b71dd3dbc79eba9de5b10e352d62554398a
wass3r <1301201+wass3r@users.noreply.github.com>
2023-11-12 08:06:25 -0600

    tweak solution, add commentary

--- executor/linux/build.go
+++ executor/linux/build.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/go-vela/types/constants"
 	"github.com/go-vela/types/library"
+	"github.com/go-vela/types/pipeline"
 	"github.com/go-vela/worker/internal/build"
 	context2 "github.com/go-vela/worker/internal/context"
 	"github.com/go-vela/worker/internal/image"
@@ -542,7 +543,57 @@ func (c *client) ExecBuild(ctx context.Context) error {
 
 		_log.SetData([]byte(""))
 
-		// temporary holder for tracking lazy secrets
+		// -------------- Lazy Loading Secrets [start] --------------
+		//
+		// this requires a small preface and brief description on
+		// how normal secrets make it into a container:
+		//
+		// 1. pull secrets
+		// 2. add them to the internal secrets map @ c.Secrets
+		// 3. call escapeNewlineSecrets() on c.Secrets
+		// 4. inject them into the container via injectSecrets()
+		// 5. call container.Substitute()
+		//
+		// 1-3 happens in PlanBuild. 4 and 5 happens in
+		// CreateStep and CreateService and for secrets added
+		// via plugin.
+		//
+		// it's important to call out that container.Substitute()
+		// can inadvertently(?) tweak the value of secrets,
+		// particularly multiline secrets and/or secrets with
+		// escaped newlines (for example). even worse, calling it
+		// multiple times on the same container can tweak
+		// them further. this is due to the json marshal/unmarshal
+		// dance that happens during the substitution process.
+		//
+		// we can't move .Substitute() here because other aspects
+		// of the build process depend on variables being
+		// substituted earlier.
+		//
+		// so, to ensure lazy loaded secrets get the same
+		// (mis)treatment and value (!) as regular secrets,
+		// we will do the following here:
+		//
+		//  1. create a temporary map for lazy loaded secrets
+		//  2. pull the lazy loaded secrets
+		//  3. add them to temporary map
+		//  4. call escapeNewlineSecrets() on temp map
+		//  5. IF there are no lazy secrets, we stop here
+		//  6. create a temporary copy of the step/container
+		//  7. remove all existing environment variables except
+		//     those needed for secret injection from the temp
+		//     copy of the step/container
+		//  8. inject the lazy loaded secrets into the
+		//     temp step/container
+		//  9. call .Substitute on the temp step/container
+		// 10. move the lazy loaded secrets over to the
+		//     actual step/container
+		//
+		// this will ensure the lazy loaded secrets return
+		// the same value as they would as regular secrets
+		// and also keep this process isolated to lazy secrets
+
+		// create a temporary map akin to c.Secrets
 		lazySecrets := make(map[string]*library.Secret)
 
 		// iterate through step secrets
@@ -597,35 +648,51 @@ func (c *client) ExecBuild(ctx context.Context) error {
 			}
 		}
 
-		// if we had lazy secrets, escape and inject them
+		// if we had lazy secrets, get them into the container
 		if len(lazySecrets) > 0 {
+			// create a copy of the current step/container
+			tmpStep := new(pipeline.Container)
+			*tmpStep = *_step
+
+			c.Logger.Debug("clearing environment in temp step/container")
+			// empty the environment
+			tmpStep.Environment = map[string]string{}
+			// but keep VELA_BUILD_EVENT as it's used in injectSecrets
+			if _, ok := _step.Environment["VELA_BUILD_EVENT"]; ok {
+				tmpStep.Environment["VELA_BUILD_EVENT"] = _step.Environment["VELA_BUILD_EVENT"]
+			}
+
 			c.Logger.Debug("escaping newlines in lazy loaded secrets")
 			// escape newlines for secrets loaded on step_start
 			escapeNewlineSecrets(lazySecrets)
 
 			c.Logger.Debug("injecting lazy loaded secrets")
 			// inject secrets for container
-			err = injectSecrets(_step, lazySecrets)
+			err = injectSecrets(tmpStep, lazySecrets)
 			if err != nil {
 				return err
 			}
 
 			c.Logger.Debug("substituting container configuration after lazy loaded secret injection")
-			// ℹ️ this is a repeat call for substitute which has been
-			// called already in CreateStep (executor/linux/step.go)
-			//
-			// since this method can (inadvertently?) modify secrets
-			// we have to call it again to treat lazy loaded secrets
-			// the same way as regular secrets
-			//
 			// substitute container configuration
 			//
 			// https://pkg.go.dev/github.com/go-vela/types/pipeline#Container.Substitute
-			err = _step.Substitute()
+			err = tmpStep.Substitute()
 			if err != nil {
 				return err
 			}
+
+			c.Logger.Debug("merge lazy loaded secrets into container")
+			// merge lazy load secrets into original container
+			err = _step.MergeEnv(tmpStep.Environment)
+			if err != nil {
+				return fmt.Errorf("failed to merge environment")
+			}
+
+			// clear out temporary var
+			tmpStep = nil
 		}
+		// -------------- Lazy Loading Secrets [end] --------------
 
 		c.Logger.Infof("planning %s step", _step.Name)
 		// plan the step
